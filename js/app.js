05d2676dac2d0eaae8f1c17cc261b76ad68ae5db
aldin24 <aldin.muratovic@stu.ibu.edu.ba>
2018-05-25 15:35:43 +0200

    commit10

--- js/app.js
+++ js/app.js
@@ -1,10 +1,46 @@
-var app = angular.module('sarajevo', ['ui.bootstrap']);
+
+
+var app = angular.module('sarajevo', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // home page ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'pages/home.html'
+        })
+
+        // explore page =================================
+        .state('explore', {
+            url: '/explore',
+            templateUrl: 'pages/explore.html'
+        })
+
+    // attractions page =================================
+        .state('explore.attractions', {
+            url: '/attractions',
+            templateUrl: 'pages/atttractions.html'
+        })
+
+        // events page =================================
+        .state('explore.events', {
+            url: '/events',
+            templateUrl: 'pages/events.html'
+        });
+
+});
+
+
+
+
+
 
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-
+$scope.name = 'Aldin';
 }]);
-
-
-
